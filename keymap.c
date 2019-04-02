c6b984b68754bcfcf1ae5238efbaae64bce6010a
Simon Rei <me@dieideeistgut.de>
2019-04-02 12:58:43 +0200

    stuffy
    Signed-off-by: Simon Rei <me@dieideeistgut.de>

--- keymap.c
+++ keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [1] = LAYOUT_60_iso( /* FN */
     KC_GESC,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL ,\
-    RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  RGB_MOD,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RESET     ,          \
+    RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  RGB_MOD,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RESET     ,       \
     MO(1),    RGB_HUD,  RGB_SAD,  RGB_VAD,  RGB_RMOD, RGB_SPD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,\
     KC_TRNS,  MO(1),    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,\
     KC_VOLU,  KC_VOLD,  KC_MUTE,                                KC_TRNS,                                KC_RGUI,  KC_RALT,  KC_TRNS,  KC_LCTL),
